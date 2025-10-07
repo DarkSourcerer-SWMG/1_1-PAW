984feb3e543fe4bf0822a42f423aed1cd4629b59
Bartosz Zając <158461898+DarkSourcerer-SWMG@users.noreply.github.com>
2025-10-07 11:25:56 +0200

    1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 (function () {
-  //TODO
+  const button=document.getElementById('ex1_button');
+
+ button.addEventListener('click', ()=>{
+  const liczby = Array.from({length:10},(_,i)=>i).join(',');
+   document.getElementById('ex1_content').textContent=liczby;
+ })
 })();
